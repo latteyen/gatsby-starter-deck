a8ddb81947cacf474d28da486d2ee8fc5c7148d4
latteyen <38789408+latteyen@users.noreply.github.com>
2019-10-08 19:41:34 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    name: `Fabian Schultz`,
-    title: `Gatsby Deck`,
-    date: `July 30, 2018`,
+    name: `ACE Marketing Solutions`,
+    title: `Website Presentation`,
+    date: `October 09, 2019`,
   },
   plugins: [
     `gatsby-plugin-layout`,
